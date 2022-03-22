98a67c99ed790453446331f91823a8d9339ce0b0
hugosenari <hugosenari@gmail.com>
2022-03-22 00:26:55 -0300

    chore: re-index ../nimpkgs/c/csfml/v2_2_0

--- v2_2_0/flake.nix
+++ v2_2_0/flake.nix
@@ -9,7 +9,7 @@
   
   inputs.src-csfml-v2_2_0.flake = false;
   inputs.src-csfml-v2_2_0.owner = "oprypin";
-  inputs.src-csfml-v2_2_0.ref   = "refs/tags/v2.2.0";
+  inputs.src-csfml-v2_2_0.ref   = "v2_2_0";
   inputs.src-csfml-v2_2_0.repo  = "nim-csfml";
   inputs.src-csfml-v2_2_0.type  = "github";
   
